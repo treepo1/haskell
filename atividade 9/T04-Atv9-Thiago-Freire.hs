072013982bc3fd3eb13bc4bf9f000d09944611e4
treepo1 <78393422+treepo1@users.noreply.github.com>
2021-02-03 16:25:10 -0300

    Update T04-Atv9-Thiago-Freire.hs

--- atividade 9/T04-Atv9-Thiago-Freire.hs
+++ atividade 9/T04-Atv9-Thiago-Freire.hs
@@ -1,22 +1,20 @@
 --q1
 import Data.Char
 
+transformar :: (a -> t) -> [a] -> [t]
+transformar f [] = []
+transformar f (x:xs) = (f x) : (transformar f xs)
+
 dif :: Int 
 dif = (ord 'a') - (ord 'A')
 
-maiuscMin a = chr((ord a) + dif)
-minuscMai a = chr((ord a) - dif)
+all_to_min a 
+  |elem a ['a'..'z'] = a
+  |otherwise = chr((ord a) + dif)
 
-transform_all_to_min [] = []
-transform_all_to_min (a:x)
-  |elem a ['A'..'Z'] == True = maiuscMin a : transform_all_to_min x
-  |otherwise = a: transform_all_to_min x
-
-transform_all_to_maiusc [] = []
-transform_all_to_maiusc (a:x)
-  |elem a ['a'..'z'] == True = minuscMai a : transform_all_to_maiusc x
-  |otherwise = a: transform_all_to_maiusc x
-
+all_to_mai a 
+  |elem a ['A'..'Z'] = a
+  |otherwise = chr((ord a) - dif)
 
 dic_10 = [(0,"zero"),(1,"um"),(2,"dois"),(3,"tres"),(4,"quatro"),(5,"cinco"),(6,"seis"),(7,"sete"),(8,"oito"), (9,"nove")]
 
@@ -27,10 +25,8 @@ traduzaux [] _ = []
 traduzaux (a:x) n 
   |n == (primeiro a) = segundo a
   |otherwise = traduzaux x n 
- 
-traduz n = traduzaux dic_10 n 
 
-transformar_num_to_string n = map traduz n 
+num_to_string n = traduzaux dic_10 n 
 
 --q2 
 total f n 
@@ -39,21 +35,25 @@ total f n
 
 --q3
 mapfiltrar p f x = map f (filter p x)
+
 eh_par n = (mod n 2 == 0)
+
 vezes_3 n = n*3
 
-
 --q4
-eh_primo n 
- | n < 1 = error "nao eh um numero positivo"
- | n == 1 = False
- | otherwise = verifPrimo n == n
+primo n 
+ |n == 1 = False
+ |n < 1 = error "nao eh um numero positivo"
+ |otherwise = check_primo n == n
 
-verifPrimo n = verifPrimoIni n 2
+check_primo a = check_div a 2
 
-verifPrimoIni n c 
- |resto n c = c
- |c^2 > n = n
- |otherwise = verifPrimoIni n (c+1)
+check_div x y
+ |divisivel x y = y
+ |y^2 > x = x
+ |otherwise = check_div x (y+1)
 
-resto n d = mod n d == 0
+divisivel c d = mod c d == 0
+
+
+
